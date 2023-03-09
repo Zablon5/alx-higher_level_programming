8b40e4dcf283fa2f5ad26277e79d44a77801e855
Zerihun Girma <zgirma74@gmail.com>
2023-03-09 13:34:35 -0800

    added 4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    file = dir(hidden_4)
+    length = len(file)
+    for i in range(0, length):
+        if file[i][0:2] != "__":
+            print(file[i])
