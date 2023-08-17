c315ed4fe3a085025663ea68e95803218f2fb96f
Zablon5 <zgirma74@gmail.com>
2023-08-17 14:22:49 +0300

    edit 0-square_matrix_simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,12 +4,4 @@ def square_matrix_simple(matrix=[]):
     a function that returns
     the squre of a givrn matrix
     """
-    n = len(matrix)
-    m = len(matrix[0])
-    new_matrix = []
-    for i in range(n):
-        current=[]
-        for j in matrix[i]:
-            current.append(j*j)
-        new_matrix.append(current)   
-    return new_matrix     
+    return list(map(lambda row:(map(lambda col:col*col,row),matrix)))
