004ac088d7bed08ba58723f95b4ef72a0fac938d
Zablon5 <zgirma74@gmail.com>
2023-08-17 16:33:00 +0300

    different element

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    return set_1 | set_2
+    set12 = set_1 | set_2
+    result_set = set(filter(lambda i: i not in set_1 or i not in set_2, set12))
+    return (result_set)
