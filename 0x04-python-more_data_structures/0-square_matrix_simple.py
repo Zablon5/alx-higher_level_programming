e15d75d8115838fb162875c612c5e09a9002a38f
Zablon5 <zgirma74@gmail.com>
2023-08-17 14:24:40 +0300

    bug fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,4 @@ def square_matrix_simple(matrix=[]):
     a function that returns
     the squre of a givrn matrix
     """
-    return list(map(lambda row:(map(lambda col:col*col,row),matrix)))
+    return (list(map(lambda row:(list(map(lambda col:col*col,row))),matrix)))
