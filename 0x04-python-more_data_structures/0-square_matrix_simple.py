32c98b017895f195b589acc65f0bb04914e3db38
Zerihun Girma <zgirma74@gmail.com>
2023-08-18 22:02:55 +0300

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    """
-    a function that returns
-    the squre of a givrn matrix
-    """
-    return (list(map(lambda row:(list(map(lambda col:col*col,row))),matrix)))
-    
--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-def only_diff_elements(set_1, set_2):
-    set12 = set_1 | set_2
-    result_set = set(filter(lambda i: i not in set_1 or i not in set_2, set12))
-    return (result_set)
